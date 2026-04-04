bb9585e26bba4cc7c503f9e8f9778e85d52551da
RajadorDev <rajadordeveloper@gmail.com>
2026-04-04 13:39:19 -0300

    Added session controller

---
+++ src/betterauth/session/SessionController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare (strict_types=1);
+ 
+/***
+ * 
+ * ██████╗ ███████╗████████╗████████╗███████╗██████╗      █████╗ ██╗   ██╗████████╗██╗  ██╗
+ * ██╔══██╗██╔════╝╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗    ██╔══██╗██║   ██║╚══██╔══╝██║  ██║
+ * ██████╔╝█████╗     ██║      ██║   █████╗  ██████╔╝    ███████║██║   ██║   ██║   ███████║
+ * ██╔══██╗██╔══╝     ██║      ██║   ██╔══╝  ██╔══██╗    ██╔══██║██║   ██║   ██║   ██╔══██║
+ * ██████╔╝███████╗   ██║      ██║   ███████╗██║  ██║    ██║  ██║╚██████╔╝   ██║   ██║  ██║
+ * ╚═════╝ ╚══════╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝
+ *   
+ * Created by:
+ * 
+ * Rajador: https://github.com/RajadorDev
+ * 
+ * Bietio: https://github.com/Bietio
+ * 
+ * NATANBX0: https://github.com/NATANBX0
+ * 
+**/
+
+namespace betterauth\session;
+
+use pocketmine\Player;
+use SmartCommand\utils\SingletonTrait;
+
+final class SessionController 
+{
+
+    use SingletonTrait;
+
+    /**
+     * @param Player $player
+     * @return boolean
+     */
+    public function isLoggedIn(Player $player) : bool
+    {
+        
+    }
+}
