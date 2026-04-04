ac350df592022f43a50ff93002b11e0ccab1d4c9
Bietio <consoler123456789@gmail.com>
2026-04-04 14:19:39 -0300

    Mudança: area do listener criada

---
+++ src/betterauth/listener/AuthListener.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace betterauth\listener;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+
+class AuthListener implements Listener
+{
+
+    public function onMove(PlayerMoveEvent $event) 
+    {
+        
+    }
+}
