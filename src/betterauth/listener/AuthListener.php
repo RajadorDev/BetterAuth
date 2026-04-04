0c81350bfc83cac8bc4825d6faa8f9aee3fb1a66
Bietio <consoler123456789@gmail.com>
2026-04-04 14:33:40 -0300

    Mudança: Bloqueio da movimentação caso não tiver logado

--- src/betterauth/listener/AuthListener.php
+++ src/betterauth/listener/AuthListener.php
@@ -2,14 +2,30 @@
 
 namespace betterauth\listener;
 
+use betterauth\session\SessionController;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 
 class AuthListener implements Listener
 {
 
+    /** @var SessionController */
+    private $session;
+
+    public function __construct()
+    {
+        $this->session = new SessionController();
+    }
+
     public function onMove(PlayerMoveEvent $event) 
     {
-        
+        $player = $event->getPlayer();
+
+        if (!$this->session->isLoggedIn($player)) 
+        {
+            $player->sendMessage("Need logar para walk");
+
+            $event->setCancelled(true);
+        }
     }
 }
