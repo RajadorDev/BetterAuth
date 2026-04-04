3727801dc051567c3a08410e9f20405293924381
Bietio <consoler123456789@gmail.com>
2026-04-04 18:13:49 -0300

    Mudança: Mais eventos cancelados

--- src/betterauth/listener/AuthListener.php
+++ src/betterauth/listener/AuthListener.php
@@ -3,13 +3,24 @@
 namespace betterauth\listener;
 
 use betterauth\Loader;
+
 use betterauth\session\SessionController;
+
 use betterauth\utils\Settings;
 use betterauth\utils\SystemMessages;
+
+use pocketmine\Player;
+
 use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+
 use pocketmine\event\Listener;
+
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
 
 final class AuthListener implements Listener
 {
@@ -28,6 +39,25 @@ final class AuthListener implements Listener
         $this->session = $session;
         $this->message = Loader::getInstance()->getMessages();
         $this->settings = Loader::getInstance()->getSettings();
+    }
+
+    public function onPreLogin(PlayerPreLoginEvent $event) 
+    {
+        $player = $event->getPlayer();
+
+        $event->setKickMessage($this->message->get("screen-cant-join", "{player_name}", $player->getName()));
+
+        if ($this->session->getSessionByUsername($player->getName()) !== null) 
+        {
+            $event->setCancelled(true);
+        }
+    }
+
+    public function onLogin(PlayerLoginEvent $event)
+    {
+        $player = $event->getPlayer();
+
+        
     }
 
     public function onMove(PlayerMoveEvent $event) 
@@ -75,6 +105,28 @@ final class AuthListener implements Listener
             $this->message->sendCooldownMessage($player, "interation-not-logged", $this->settings->getBlockEventsMessageCooldown());
 
             $event->setCancelled(true);
+        }
+    }
+
+    public function onDamage(EntityDamageEvent $event)
+    {
+        $entity = $event->getEntity();
+
+        if ($entity instanceof Player && !$this->session->isLoggedIn($entity)) 
+        {
+            $event->setCancelled(true);
+
+            return;
+        }
+
+        if ($event instanceof EntityDamageByEntityEvent) 
+        {
+            $entityDamager = $event->getDamager();
+
+            if ($entityDamager instanceof Player && !$this->session->isLoggedIn($entity)) 
+            {
+                $event->setCancelled(true);
+            }
         }
     }
 }
