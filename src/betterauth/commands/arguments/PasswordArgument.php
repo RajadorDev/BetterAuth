7589907cd44b4a37c2817b71d521be0d878a6e64
NATANBX0 <natanbx0@gmail.com>
2026-04-05 23:31:48 +0100

    folder argument rename

---
+++ src/betterauth/commands/arguments/PasswordArgument.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/***
+ * 
+ * ██████╗ ███████╗████████╗████████╗███████╗██████╗      █████╗ ██╗   ██╗████████╗██╗  ██╗
+ * ██╔══██╗██╔════╝╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗    ██╔══██╗██║   ██║╚══██╔══╝██║  ██║
+ * ██████╔╝█████╗     ██║      ██║   █████╗  ██████╔╝    ███████║██║   ██║   ██║   ███████║
+ * ██╔══██╗██╔══╝     ██║      ██║   ██╔══╝  ██╔══██╗    ██╔══██║██║   ██║   ██║   ██╔══██║
+ * ██████╔╝███████╗   ██║      ██║   ███████╗██║  ██║    ██║  ██║╚██████╔╝   ██║   ██║  ██║
+ * ╚═════╝ ╚══════╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝
+ *   
+ * Created by:
+ * 
+ * Rajador: https://github.com/RajadorDev
+ * 
+ * Bietio: https://github.com/Bietio
+ * 
+ * NATANBX0: https://github.com/NATANBX0
+ * 
+ **/
+
+namespace betterauth\commands\arguments;
+
+use betterauth\Loader;
+use pocketmine\utils\TextFormat;
+use SmartCommand\command\argument\BaseArgument;
+use SmartCommand\message\CommandMessages;
+
+class PasswordArgument extends BaseArgument
+{
+    public function __construct(string $name, bool $required)
+    {
+        return parent::__construct(
+            $name,
+            'string',
+            $required,
+            function (&$given) {
+                if (strlen($given) > Loader::getInstance()->getSettings()->getMaxPasswordLength()) {
+                    return false;
+                }
+                if (strlen($given) < Loader::getInstance()->getSettings()->getMinPasswordLength()) {
+                    return false;
+                }
+            }
+        );
+    }
+
+    public function getWrongMessage(CommandMessages $commandMessages, string $argumentUsed): string
+    {
+        $maxChar = Loader::getInstance()->getSettings()->getMaxPasswordLength();
+        $minChar = Loader::getInstance()->getSettings()->getMinPasswordLength();
+        $commandMessages->set(CommandMessages::INVALID_ARGUMENT, TextFormat::RED . "Sua senha precisa ter no mínimo $minChar caracteres e no máximo $maxChar caracteres!");
+        return parent::getWrongMessage($commandMessages, $argumentUsed);
+    }
+}
